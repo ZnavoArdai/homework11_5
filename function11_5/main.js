80c28977566e29d79b0bba0c3c07b5a9a6b9392f
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2022-05-15 18:37:22 +0300

    Add files via upload

---
+++ function11_5/main.js
@@ -0,0 +1,307 @@
+// function helloWorld(){
+//     console.log("hello world")
+// }
+
+// helloWorld();
+// helloWorld();
+// helloWorld();
+
+// 2
+
+// function max(num1,num2){
+//     num1=prompt("nume1");
+//     num2=prompt("number2");
+//     if(num1>num2)
+//     {
+//         return console.log(num1)
+
+//     }
+//     else{
+//         return console.log(num2)
+//     }
+// }
+
+// max();
+
+// 3
+
+// function sum (num1,num2){
+//     num1=+prompt("nume1");
+//     num2=+prompt("number2");
+
+//     var sumOfNumbers=num1+num2;
+
+//     return console.log(sumOfNumbers) ;
+
+// }
+
+// sum();
+
+//4
+
+// function fck (num1){
+//     num1=+prompt("nume1");
+
+//     for(var i=num1-1;i>0;i--){
+
+
+// num1+=i;
+//     }
+
+//     return console.log(num1)
+
+
+// }
+
+// fck();
+
+
+//5
+
+
+// var num1=5;
+// var num2=8;
+// function switchNums(){
+// var num3=0; 
+// console.log("f"+num1,num2)
+// num3=num1;
+// num1=num2;
+// num2=num3;
+
+// return console.log(num1,num2)
+
+// }
+// switchNums();
+
+//6
+// var star="*";
+
+// console.log("*"*5)
+
+// function stars(num){
+
+//     for(var i=num;i>0;i--){
+// var star="*";
+
+// num--;
+// console.log(star*num)
+
+//     }
+
+// }
+
+// stars(4);
+
+// var i=0;
+// var max=0;
+// var min=1;
+// var temp=0;
+
+// while(i<5){
+// var num=+prompt("put number");
+
+// temp=num;
+// if(max<num){
+//     max=num;
+// }
+
+// //     if(temp<max&&temp>min)
+// //     {
+// //         min=temp
+// //     }
+// // }
+
+// else if(num<min
+// )
+// {
+//     min=num;
+// }
+// i++;
+
+// }
+
+//  console.log(max)
+//  console.log(min)
+
+function israel(){
+
+    console.log("israel")
+}
+
+israel();
+
+function firstName(){
+    console.log("znavo")
+}
+firstName()
+
+function fullNumber(num){
+    num=4;
+
+    return console.log(num);
+}
+
+fullNumber();
+
+
+function fiveNum(num){
+num=5;
+var i=0;
+while( i<num){
+console.log(num);
+i++;
+}
+}
+fiveNum();
+
+// function gettext(text){
+//     var num=prompt("please enter"+text)
+
+// }
+
+function Prompttext(text){
+    return prompt("enter"+text)
+
+}
+function Promptnum(text){
+    return parseInt(prompt("enter"+text))
+
+}
+
+//5
+
+// function avgAndSum(num1,num2,num3,num4){
+// num1=parseInt(getPrompt());
+// num2=parseInt(getPrompt())
+// num3=parseInt(getPrompt());
+// num4=parseInt(getPrompt());
+
+//  var sum=num1+num2+num3+num4;
+
+// var avg=sum/4;
+
+// return console.log(avg,sum)
+
+
+// }
+
+// avgAndSum();
+
+
+// function squer(num1,num2){
+// num1=parseInt(getPrompt());
+// num2=parseInt(getPrompt());
+
+// return console.log(num1*num2) ;
+// }
+
+// squer();
+
+
+// function switchnums(num1,num2){
+
+// num3=0;
+
+// console.log(num1,num2);
+
+
+
+
+// num3=num1;
+
+// num1=num2;
+// num2=num3;
+
+// return console.log(num1,num2);
+
+// }
+
+// switchnums(4,8);
+
+// 10
+// function sumAndAvg(num){
+// var i=0;
+// var sum=0;
+// var avg=0;
+// while(i<4){
+// num=parseInt(prompt("put numbers"));
+
+// sum=sum+num;
+
+
+// avg=sum/i;
+
+
+//     i++;
+// }
+
+// return console.log(sum+"-----"+avg)
+// }
+
+// sumAndAvg();
+
+//11
+
+// function withAndLength(num1,num2){
+// num1=parseInt(prompt("enter number 1"));
+// num2=parseInt(prompt("enter number 2"));
+
+// return console.log(num1*num2);
+
+
+// }
+
+
+// withAndLength();
+
+//12
+
+// function swithnums(num1,num2){
+// num1=Promptnum();
+// num2=Promptnum();
+ 
+// console.log(num1+" "+num2)
+
+// var num3=0;
+// num3=num1;
+// num1=num2;;
+// num2=num3;
+
+// return console.log(num1+" "+num2)
+// }
+
+
+// swithnums();
+
+
+// function stars(num){
+//     var i;
+//     var j;
+//   for(var i=1;i<num;i++){
+// for(j=1;j<=i;j++){
+//     console.log("*")
+
+// }
+// }
+// }
+// stars(4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
